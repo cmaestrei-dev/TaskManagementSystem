a2e5c3d2c9951bd2792e418471c17b0b2db87dd6
Camilo <maestre765@gmail.com>
2026-02-06 19:18:19 -0500

    Create Participant model and migration. Add has_many :through associations to User and Task models to enable many-to-many relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tasks, foreign_key: 'owner_id', dependent: :destroy
+  has_many :owned_tasks, foreign_key: 'owner_id', class_name: 'Task', dependent: :destroy
+  has_many :participations, class_name: 'Participant'
+  has_many :tasks, through: :participations
 end
